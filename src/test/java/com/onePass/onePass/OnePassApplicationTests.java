f75ad96ca3b1ea374e36439f6147ff60256b7e38
Gowtham-GG <gggpalani2002@gmail.com>
2024-02-02 23:16:29 +0530

    Base

---
+++ src/test/java/com/onePass/onePass/OnePassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onePass.onePass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
