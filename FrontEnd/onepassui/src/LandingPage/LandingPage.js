05abef8c93ddac4b04b22f3e99aaab672a3e8797
Gowtham-GG <gggpalani2002@gmail.com>
2024-03-16 11:20:17 +0530

    dash ui almost ready

--- FrontEnd/onepassui/src/LandingPage/LandingPage.js
+++ FrontEnd/onepassui/src/LandingPage/LandingPage.js
@@ -1,9 +1,47 @@
 import React from "react";
 
 
-function LandingPage()
+function LandingPage()  
 {
-    return(<h1>Hi</h1>)
+    return(
+
+<div class="body">
+      
+
+      <div class="outer">
+  <div class="outer-animation">
+  <div class="inner">
+    <div class="orange"><div class="inner-orange-opaque">
+<div class="inner-orange-transparent"></div>
+</div></div>
+
+
+<div class="red"><div class="inner-red-opaque">
+<div class="inner-red-transparent"></div>
+</div></div>
+
+
+<div class="blue"><div class="inner-blue-opaque"><div class="blue-animation">
+<div class="inner-blue-transparent"></div>
+</div></div></div>
+
+  </div></div>
+
+  <div class="ring-button-outer" id="button-1"><div class="ring-button-inner"></div></div>
+
+  <div class="ring-button-outer" id="button-3"><div class="ring-button-inner"></div></div>
+
+
+</div>
+
+
+      </div>
+
+
+
+
+
+    )
 }
 
 export default LandingPage;
